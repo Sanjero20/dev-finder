8f394c486a6b9173940600f8af12ce3571ed4cee
Sanjero20 <gelosantos72@gmail.com>
2024-05-03 20:48:58 +0800

    add right padding to search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,16 +2,16 @@ import { Search } from "lucide-react";
 
 function Searchbar() {
   return (
-    <section className="bg-container dark:bg-container-dark relative flex items-center justify-between rounded-lg p-2 drop-shadow-lg">
-      <Search className="text-primary ml-2" size={24} />
+    <section className="relative flex items-center justify-between rounded-lg bg-container p-2 drop-shadow-lg dark:bg-container-dark">
+      <Search className="ml-2 text-primary" size={24} />
 
       <input
         type="text"
         placeholder="Search Github username"
-        className="absolute left-0 h-full w-full rounded-lg bg-transparent pl-14"
+        className="absolute left-0 h-full w-full text-ellipsis rounded-lg bg-transparent pl-14 pr-28"
       />
 
-      <button className="bg-primary z-10 rounded px-4 py-2 font-bold text-white">
+      <button className="z-10 rounded bg-primary px-4 py-2 font-bold text-white">
         Search
       </button>
     </section>
