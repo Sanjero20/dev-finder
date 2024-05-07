87a3cdbbaf833a6bc2f8b74dd6e3d04dc75e31f5
Sanjero20 <gelosantos72@gmail.com>
2024-05-07 22:59:44 +0800

    fix date formatting
    instead of using getDay() which converts to the day of the week, use
    getDate() instead for proper conversion.

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -3,7 +3,7 @@ export function formatDate(str: string) {
 
   if (!date) return "";
 
-  const day = date.getDay().toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
   const month = date.toLocaleDateString("en-US", { month: "short" });
   const year = date.getFullYear();
 
